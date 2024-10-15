f058406815f03a13184a8a06b172e866134c4dd8
David Sánchez <davidsanchez@Davids-MacBook-Pro-3.local>
2024-10-15 16:14:29 -0600

    commit

---
+++ UserAVL.h
@@ -0,0 +1,51 @@
+//
+// Created by David Sánchez on 15/10/24.
+//
+
+#ifndef USERAVL_H
+#define USERAVL_H
+#include <string>
+using namespace std;
+
+
+
+class User {
+public:
+    int id;
+    string name;
+    string email;
+    string creationDate;
+
+    User(int id, string name, string email, string creationDate);
+};
+
+
+class Node {
+public:
+    User* user;
+    Node* left;
+    Node* right;
+    int height;
+
+    Node(User* user);
+};
+
+class AVLTree {
+public:
+    Node* root;
+
+    AVLTree(); // Constructor del árbol AVL
+
+    int height(Node* node);
+    int getBalance(Node* node);
+    Node* rightRotate(Node* y);
+    Node* leftRotate(Node* x);
+    Node* insert(Node* node, User* user);
+    User* search(Node* node, int id);
+    void inorder(Node* node);
+};
+
+
+
+
+#endif //USERAVL_H
